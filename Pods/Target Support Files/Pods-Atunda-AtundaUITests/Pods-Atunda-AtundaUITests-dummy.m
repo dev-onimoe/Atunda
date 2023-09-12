51c9be81f8a97673d0faa03b9443467157cb69c6
Mas'ud <68595786+dev-onimoe@users.noreply.github.com>
2023-09-12 11:55:16 +0100

    Atunda push 3

---
+++ Pods/Target Support Files/Pods-Atunda-AtundaUITests/Pods-Atunda-AtundaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Atunda_AtundaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Atunda_AtundaUITests
+@end
---
+++ Pods/Target Support Files/Pods-AtundaTests/Pods-AtundaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AtundaTests : NSObject
+@end
+@implementation PodsDummy_Pods_AtundaTests
+@end
